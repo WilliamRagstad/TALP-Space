9b0845463bffb1531442a970db8d161e64e45ebc
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-22 01:45:57 +0100

    Add inside bar

--- script/world.js
+++ script/world.js
@@ -3,26 +3,39 @@ const rooms = {};
 
 function initializeWorld() {
   const desert_outsideBase = new Room(
-    "Desert outside hangar",
+    "Desert outside bar",
     "assets/inspiration/sq2.jpg",
   );
   addRoom(desert_outsideBase);
+
   const desert_insideBase = new Room(
-    "Desert inside hangar",
-    "assets/inspiration/36b5.gif",
+    "Desert inside bar",
+    "assets/inspiration/36b5.gif"
   );
   addRoom(desert_insideBase);
 
-  goto(desert_outsideBase.id);
+  desert_outsideBase.addListener(Action.Enter, (args) => {
+    if ([Keyword.Building.Bar].includes(args[0])) {
+      goto(desert_insideBase);
+      return { status: true, message: "You enter the " + args[0] + "." };
+    }
+    return { status: false, message: "Unknown building." };
+  });
+  desert_insideBase.addListener(Action.Exit, () => {
+	  goto(desert_outsideBase);
+	  return { status: true, message: "You exit the bar." };
+	});
+
+  goto(desert_outsideBase);
 }
 
 function addRoom(room) {
   rooms[room.id] = room;
 }
 
-function goto(roomId) {
-  if (currentRoom) currentRoom.notify("exit", roomId);
-  currentRoom = rooms[roomId];
+function goto(room) {
+  if (currentRoom) currentRoom.notify("end", room.id);
+  currentRoom = rooms[room.id];
   currentRoom.draw();
-  currentRoom.notify("enter", this.name);
+  currentRoom.notify("beginning", this.name);
 }
